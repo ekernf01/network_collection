3e3563aefdb2f767ca570f0ea646cc4ba2a78c96
Eric Kernfeld <eric.kern13@gmail.com>
2024-08-01 16:26:20 -0400

    Add CellOracle mouse and zebrafish networks

--- setup/celloracle.py
+++ setup/celloracle.py
@@ -1,16 +1,23 @@
-# This assumes os.environ["GRN_PATH"] points to the networks folder adjacent to the README. 
+# This script should work in any environment where celloracle is installed. 
+import os
 import celloracle as co
 import pandas as pd
-x = co.data.load_human_promoter_base_GRN()
-x.drop(["peak_id"], axis = 1, inplace=True)
-x = pd.melt(x, id_vars = "gene_short_name")
-x.set_axis(["target", "regulator", "is_present"], axis=1, inplace=True)
-x = x.loc[x['is_present'] == 1]
-x.is_present *= -1
-write_to = os.path.join(os.environ["GRN_PATH"], "celloracle_human", "networks", "human_promoters.csv")
-try:
-    os.makedirs(os.path.dirname(write_to))
-except:
-    pass
-x.to_csv(write_to, header = False, index = False)
-os.system("gzip " + write_to)
+os.environ["GRN_PATH"] = "../network_collection/networks"
+def fix_format(x, net, subnet):
+    x.drop(["peak_id"], axis = 1, inplace=True)
+    x = pd.melt(x, id_vars = "gene_short_name")
+    x.set_axis(["target", "regulator", "weight"], axis=1, inplace=True)
+    x = x.loc[x['weight'] == 1]
+    x["weight"] = -1
+    x = x[["regulator", "target", "weight"]]
+    print(x.head())
+    write_to = os.path.join(os.environ["GRN_PATH"], net, "networks", subnet)
+    try:
+        os.makedirs(os.path.dirname(write_to))
+    except:
+        pass
+    x.to_parquet(write_to)
+
+fix_format(x = co.data.load_human_promoter_base_GRN(), net = "celloracle_human", subnet = "human_promoters.parquet")
+fix_format(x = co.data.load_mouse_promoter_base_GRN(), net = "celloracle_mouse", subnet = "all_promoters.parquet")
+fix_format(x = co.data.load_zebrafish_promoter_base_GRN(), net = "celloracle_zebrafish", subnet = "all_promoters.parquet")
